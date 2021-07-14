8656c6b3686fb892da0926df0898326189ce6fcd
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2021-07-14 14:26:21 +0300

    Release component blob to version 1.0.0.

--- blob/src/tests-setup/interface.ts
+++ blob/src/tests-setup/interface.ts
@@ -1,5 +1,5 @@
 import * as storage from "@azure/storage-blob";
-import * as identity from "@azure/identity";
+import * as identity from "@azure/core-auth";
 import test, { TestInterface } from "ava";
 
 export const thisTest = test as TestInterface<BlobSinkTestContext>;
